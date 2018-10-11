e51c6c69ee9d80c858e789b7d745976b146da872
Juan Bracamontes <juanbracamontes02@gmail.com>
2018-10-10 19:34:20 -0700

    Agregando menu lateral

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { Platform } from 'ionic-angular';
+import { Platform , MenuController} from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
@@ -11,12 +11,26 @@ import {LoginPage} from "../pages/login/login";
 export class MyApp {
   rootPage:any = LoginPage;
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+  constructor(
+    platform: Platform,
+    statusBar: StatusBar,
+    splashScreen: SplashScreen,
+    private menuCtrl:MenuController
+  ) {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       statusBar.styleDefault();
       splashScreen.hide();
     });
+  }
+
+  closeMenu(){
+    this.menuCtrl.close();
+  }
+
+  endSesion(){
+    this.rootPage = LoginPage;
+    this.menuCtrl.close();
   }
 }
